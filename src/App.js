8934f2aa4f26def88bfd4e2ec56a0cbb6f5da980
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-07 17:54:34 +0800

    feat: abstract catalogue component and complete basic style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import "./App.css";
+import Catalogue from "./component/category/Catalogue";
 
 function App() {
   return (
     <div className="App">
       <h1>产品列表</h1>
       <p>目前项目在跟进的产品</p>
-      <div>
-        <div>
-          <i className="iconfont icon-kuaisugaoxiao"></i>
-          <span>快速开始</span>
-        </div>
-        <div>
-          <i className="iconfont icon-info-circle"></i>
-          <span>产品简介</span>
-        </div>
-        <div>
-          <i className="iconfont icon-wendang"></i>
-          <span>产品文档</span>
-        </div>
-      </div>
+      <Catalogue />
     </div>
   );
 }

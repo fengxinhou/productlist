3f065ae667f7212527851fa779dd27b17b61278a
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-08 14:55:54 +0800

    feat: complete the initial rendering of productList

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -1,36 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./productList.css";
 import axios from "axios";
 
 function ProductList(props) {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/lists").then((res) => {
-      console.log(res.data);
+      setProducts(res.data);
     });
   }, []);
+
   return (
     <div className="main">
       <div className="newCard">
         <span>新增产品</span>
       </div>
-      <div className="card">
-        <div className="card_product">
-          <i className="iconfont">&#xe879;</i>
-          <div className="content">
-            <span>title</span>
-            <p>
-              支付宝（中国）网络技术有限公司成立于2004年，是国内的第三方支付平台，致力于为企业和个人提供'简单、安全、快速、便捷'的支付解决方案。
-              支付宝公司从2004年建立开始，始终以'信任'作为产品和服务的核心。
-              旗下有'支付宝'与'支付宝钱包'两个独立品牌。
-              自2014年第二季度开始成为当前全球最大的移动支付厂商。
-            </p>
+      {products.map((item) => {
+        return (
+          <div className="card" key={item.id}>
+            <div className="card_product">
+              <i className="iconfont">{item.icon}</i>
+              <div className="content">
+                <span>{item.title}</span>
+                <p>{item.content}</p>
+              </div>
+            </div>
+            <div className="option">
+              <button>编辑</button>
+              <button>删除</button>
+            </div>
           </div>
-        </div>
-        <div className="option">
-          <button>编辑</button>
-          <button>删除</button>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }

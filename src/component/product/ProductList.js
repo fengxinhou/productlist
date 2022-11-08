3e07c71ec68ee57fd532b82b82ee61dfb1c8e729
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-08 10:51:52 +0800

    feat: add json-server and add axios

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./productList.css";
+import axios from "axios";
 
 function ProductList(props) {
+  useEffect(() => {
+    axios.get("http://localhost:3000/lists").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
   return <div className="main">卡片</div>;
 }
 

8aceadd2c74a6b47c2401a7aceff6da09cd12394
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-09 17:17:10 +0800

    refactor: delete unless code

--- src/api/products.js
+++ src/api/products.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
 const URL = "http://localhost:3000/products";
+
 export const getProduct = () => axios.get(URL).then((res) => res.data);
+
 export const addProduct = (newProduct) => {
   axios.post(URL, newProduct).then((res) => res.data);
 };
+
 export const deleteProduct = (id) =>
   axios.delete(`${URL}/${id}`).then((res) => res.status);
+
 export const updateProduct = (
   id,
   editProductUrl,

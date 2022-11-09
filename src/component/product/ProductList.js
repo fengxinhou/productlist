644cf366388744ef7463235834424f0ffec8100a
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-09 14:48:39 +0800

    refactor:  delete unless code from productList

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -1,24 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import "./productList.css";
 import Card from "../card/Card";
 import AddModal from "../Modal/AddModal";
-import { ProductContext } from "../../App";
 
-function ProductList(props) {
-  const { addNewProduct } = props;
-  const products = useContext(ProductContext);
+function ProductList() {
   const [addModal, setAddModal] = useState(false);
-
-  const addProduct = (productUrl, productName, productDesc) => {
-    const newProduct = {
-      id: products.length > 0 ? products[products.length - 1].id + 1 : 0,
-      url: productUrl,
-      name: productName,
-      description: productDesc,
-    };
-    addNewProduct(newProduct);
-    setAddModal(false);
-  };
 
   return (
     <div className="main">
@@ -31,13 +17,15 @@ function ProductList(props) {
           新增产品
         </button>
       </div>
-      <Card products={products} />
+      <Card />
       <AddModal
         visible={addModal}
         onClose={() => {
           setAddModal(false);
         }}
-        addProduct={addProduct}
+        addProduct={() => {
+          setAddModal(false);
+        }}
       />
     </div>
   );

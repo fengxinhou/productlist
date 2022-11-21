28990b4574f6afed626669f54be9ff955374fb06
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-21 17:30:00 +0800

    test: input should have real value and update when input a new value

--- src/component/crud/addOrEdit.test.js
+++ src/component/crud/addOrEdit.test.js
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import AddOrEdit from "./AddOrEdit";
 import "@testing-library/jest-dom";
 
+const mockHandleChange = jest.fn();
 describe("AddOrEdit", () => {
   const product = {
     id: 0,
@@ -11,18 +12,46 @@ describe("AddOrEdit", () => {
   };
   const product1 = {
     id: 0,
-    url: "",
-    name: "测试初始值",
-    description: "测试初始值",
+    url: "测试url",
+    name: "测试name",
+    description: "测试description",
   };
+
   test("input should be empty initially", () => {
-    const { getByRole } = render(<AddOrEdit product={product} />);
+    const { getByRole } = render(
+      <AddOrEdit product={product} handleChangeProduct={mockHandleChange} />
+    );
     const input = getByRole("textbox", { name: "desc：" });
     expect(input).toHaveValue("");
   });
+
   test("input should have default value when initially", () => {
-    const { getByRole } = render(<AddOrEdit product={product1} />);
+    const { getByRole } = render(
+      <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
+    );
     const input = getByRole("textbox", { name: "desc：" });
-    expect(input).toHaveValue("测试初始值");
+    expect(input).toHaveValue("测试description");
+  });
+
+  test("input should update when input a new value", async () => {
+    const { getByLabelText, getByDisplayValue } = render(
+      <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
+    );
+    const hasInputValue = (e, inputValue) => {
+      return getByDisplayValue(inputValue) === e;
+    };
+
+    const input = getByLabelText("product name：");
+    fireEvent.change(input, { target: { value: "测试name" } });
+    expect(hasInputValue(input, "测试name")).toBe(true);
+  });
+
+  test("input should have a real value when test by LabelText", async () => {
+    const { getByLabelText } = render(
+      <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
+    );
+    await waitFor(() => {
+      expect(getByLabelText("avatar url：")).toHaveValue("测试url");
+    });
   });
 });

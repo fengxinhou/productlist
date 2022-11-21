54e17cc5fd56983d61655c897adc15990671cf86
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-21 17:51:10 +0800

    test: should render modal when click delete or edit modal

--- src/component/card/card.test.js
+++ src/component/card/card.test.js
@@ -1,8 +1,9 @@
-import { render } from "@testing-library/react";
+import { getByText, render, waitFor } from "@testing-library/react";
 import Card from "./Card";
 import React from "react";
 import { ProductContext } from "../../App";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("productList", () => {
   let mockProduct = [
@@ -21,7 +22,7 @@ describe("productList", () => {
         "支付宝（中国）网络技术有限公司成立于2004年，是国内的第三方支付平台，致力于为企业和个人提供'简单、安全、快速、便捷'的支付解决方案。 支付宝公司从2004年建立开始，始终以'信任'作为产品和服务的核心。 旗下有'支付宝'与'支付宝钱包'两个独立品牌。 自2014年第二季度开始成为当前全球最大的移动支付厂商。",
     },
   ];
-  const { getAllByRole, getAllByText } = render(
+  const { getAllByRole, getAllByText, getByText } = render(
     <ProductContext.Provider value={{ products: mockProduct }}>
       <Card />
     </ProductContext.Provider>
@@ -34,5 +35,23 @@ describe("productList", () => {
   test("should render two edit button when have two products", () => {
     const editBtn = getAllByText("编辑");
     expect(editBtn).toHaveLength(2);
+  });
+
+  test("should render delete modal when click delete button", async () => {
+    const deleteBtn = getAllByText("删除");
+    userEvent.click(deleteBtn[0]);
+    await waitFor(() => {
+      expect(
+        getByText("Are you sure you want to delete this product?")
+      ).toBeInTheDocument();
+    });
+  });
+
+  test("should render edit modal when click delete button", async () => {
+    const editBtn = getAllByText("编辑");
+    userEvent.click(editBtn[0]);
+    await waitFor(() => {
+      expect(getByText("Edit Product")).toBeInTheDocument();
+    });
   });
 });

aff736b3e191569f265036bda9d47376ceedc226
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-22 17:57:08 +0800

    delete: delete footer test

--- src/component/Modal/footer.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Footer from "./Footer";
-import "@testing-library/jest-dom";
-
-test("handles error for scoops and toppings routes", async () => {
-  const { getByRole, getByText } = render(<Footer />);
-  expect(getByRole("button", { name: "cancel" })).toBeInTheDocument();
-  expect(getByText("Cancel")).toBeInTheDocument();
-});

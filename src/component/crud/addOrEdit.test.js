4c6341c542b03e2b1b98c893a1ea043f16c3c07a
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-22 17:56:42 +0800

    test: should loader context when onMouseOver

--- src/component/crud/addOrEdit.test.js
+++ src/component/crud/addOrEdit.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import AddOrEdit from "./AddOrEdit";
 import "@testing-library/jest-dom";
 
@@ -40,18 +40,28 @@ describe("AddOrEdit", () => {
     const hasInputValue = (e, inputValue) => {
       return getByDisplayValue(inputValue) === e;
     };
-
     const input = getByLabelText("product name：");
     fireEvent.change(input, { target: { value: "测试name" } });
     expect(hasInputValue(input, "测试name")).toBe(true);
   });
 
-  test("input should have a real value when test by LabelText", async () => {
-    const { getByLabelText } = render(
+  test("input should loader context when onMouseOver", async () => {
+    const { getByLabelText, getByDisplayValue } = render(
       <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
     );
-    await waitFor(() => {
-      expect(getByLabelText("avatar url：")).toHaveValue("测试url");
-    });
+    const hasInputValue = (e, inputValue) => {
+      return getByDisplayValue(inputValue) === e;
+    };
+    const inputName = getByLabelText("product name：");
+    fireEvent.mouseOver(inputName);
+    expect(hasInputValue(inputName, "测试name")).toBeTruthy();
+
+    const inputUrl = getByLabelText("avatar url：");
+    fireEvent.mouseOver(inputUrl);
+    expect(hasInputValue(inputUrl, "测试url")).toBeTruthy();
+
+    const inputDesc = getByLabelText("desc：");
+    fireEvent.mouseOver(inputDesc);
+    expect(hasInputValue(inputDesc, "测试description")).toBeTruthy();
   });
 });

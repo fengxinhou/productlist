be9298cb519bf81b821cd6416feab4339ad77d22
fengxinhou <fengxin.hou@thoughtworks.com>
2022-11-23 10:31:30 +0800

    refactor: modify the test by use before and after

--- src/component/Modal/modal.test.js
+++ src/component/Modal/modal.test.js
@@ -2,15 +2,24 @@ import Modal from "./Modal";
 import { fireEvent, render } from "@testing-library/react";
 import Delete from "../crud/Delete";
 import "@testing-library/jest-dom";
+import React from "react";
 
 describe("modal and delete component", () => {
-  test("modal shows the children and a close button", () => {
-    const handleClose = jest.fn();
-    const { getByText } = render(
+  let element = null;
+  const handleClose = jest.fn();
+  beforeEach(() => {
+    element = render(
       <Modal onClose={handleClose} open={true} title="Delete Product">
         <Delete />
       </Modal>
     );
+  });
+
+  afterEach(() => {
+    element = null;
+  });
+  test("modal shows the children and a close button", () => {
+    const { getByText } = element;
     expect(getByText("Delete Product")).toBeTruthy();
     fireEvent.click(getByText(/Cancel/i));
     expect(handleClose).toHaveBeenCalledTimes(1);
--- src/component/crud/addOrEdit.test.js
+++ src/component/crud/addOrEdit.test.js
@@ -1,20 +1,15 @@
 import { fireEvent, render } from "@testing-library/react";
 import AddOrEdit from "./AddOrEdit";
 import "@testing-library/jest-dom";
+import React from "react";
 
 const mockHandleChange = jest.fn();
-describe("AddOrEdit", () => {
+describe("initially AddOrEdit", () => {
   const product = {
     id: 0,
     url: "",
     name: "",
     description: "",
-  };
-  const product1 = {
-    id: 0,
-    url: "测试url",
-    name: "测试name",
-    description: "测试description",
   };
 
   test("input should be empty initially", () => {
@@ -24,19 +19,33 @@ describe("AddOrEdit", () => {
     const input = getByRole("textbox", { name: "desc：" });
     expect(input).toHaveValue("");
   });
-
-  test("input should have default value when initially", () => {
-    const { getByRole } = render(
+});
+describe("render AddOrEdit", () => {
+  const product1 = {
+    id: 0,
+    url: "测试url",
+    name: "测试name",
+    description: "测试description",
+  };
+  let element = null;
+  beforeEach(() => {
+    element = render(
       <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
     );
+  });
+
+  afterEach(() => {
+    element = null;
+  });
+
+  test("input should have default value when initially", () => {
+    const { getByRole } = element;
     const input = getByRole("textbox", { name: "desc：" });
     expect(input).toHaveValue("测试description");
   });
 
   test("input should update when input a new value", async () => {
-    const { getByLabelText, getByDisplayValue } = render(
-      <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
-    );
+    const { getByLabelText, getByDisplayValue } = element;
     const hasInputValue = (e, inputValue) => {
       return getByDisplayValue(inputValue) === e;
     };
@@ -46,9 +55,7 @@ describe("AddOrEdit", () => {
   });
 
   test("input should loader context when onMouseOver", async () => {
-    const { getByLabelText, getByDisplayValue } = render(
-      <AddOrEdit product={product1} handleChangeProduct={mockHandleChange} />
-    );
+    const { getByLabelText, getByDisplayValue } = element;
     const hasInputValue = (e, inputValue) => {
       return getByDisplayValue(inputValue) === e;
     };
